494dfb5486c96ea2791ad38eee94690a7421b2d8
Ti Windows <>
2025-09-19 17:21:41 +0100

    did more mobile responsive designs and updated the title icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,26 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import tailwindcss from "@tailwindcss/vite";
-import { defineNuxtConfig } from 'nuxt/config'
+import { defineNuxtConfig } from "nuxt/config";
 
 export default defineNuxtConfig({
-  srcDir: 'app/',
+  srcDir: "app/",
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   runtimeConfig: {
     cfAccountId: process.env.CF_ACCOUNT_ID,
-    cfApiToken: process.env.CF_API_TOKEN
+    cfApiToken: process.env.CF_API_TOKEN,
   },
-  modules: ['@nuxt/image'],
+  modules: ["@nuxt/image"],
   app: {
     head: {
-      title: 'Precious Moments',
-      titleTemplate: '%s | Precious Moments'
-    }
+      title: "Precious Moments",
+      titleTemplate: "%s | Precious Moments",
+      link: [{ rel: "icon", type: "image/svg+xml", href: "/favicon.svg" }],
+    },
   },
   css: ["~/assets/css/main.css"],
   vite: {
-    plugins: [
-      tailwindcss(),
-    ],
+    plugins: [tailwindcss()],
   },
 });
